cdd322b53c31d0e262c3d39f8e656b5be9c6801d
Shivam Nangare <shivamnangare63@gmail.com>
2026-09-25 15:42:13 +0530

    ✅ GSQ06 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-25

--- codechef/medium/GSQ06/solution.sql
+++ codechef/medium/GSQ06/solution.sql
@@ -1,4 +1,9 @@
 /* Solution as follows */
 
-INSERT INTO employee (Employee_id,Employee_Name,Department)
-VALUES (6,'Brandon Kim','Operations');
+INSERT INTO employee(Id,Name,Age,Address)
+
+/* email id as a field is not a column of the table defined */
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30, '456 Broadway');
+
+SELECT * FROM employee;
