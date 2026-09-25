c364ce84b76991d390da0f9fb16d3bea5159aa64
Shivam Nangare <shivamnangare63@gmail.com>
2026-09-25 15:41:44 +0530

    ✅ GSQ03E | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/GSQ03E/solution.cpp
@@ -0,0 +1,6 @@
+/* Solution as follows*/
+
+/*Lets add the details of 2 employees to the table 'employee' */
+INSERT INTO employee (Employee_id,Employee_Name,Department)
+VALUES (4,'Marcus Garcia','Product'),
+       (5,'Samantha Park','Hr');
