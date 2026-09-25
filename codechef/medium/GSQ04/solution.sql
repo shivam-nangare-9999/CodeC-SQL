8421b678c320e22c55cabc3e3b32ef9cbf4793f2
Shivam Nangare <shivamnangare63@gmail.com>
2026-09-25 15:40:37 +0530

    ✅ GSQ04 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/GSQ04/solution.sql
@@ -0,0 +1,8 @@
+/* Solution as follows */
+
+CREATE TABLE employee
+(
+    Employee_id INT , /* Integer Data type assigned to the variable */
+    Employee_Name   TEXT, /* TEXT Data type assigned to the variable */
+    Department  TEXT /* TEXT Data type assigned to the variable */
+);
